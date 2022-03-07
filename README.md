080b6a67ae7c9c7c36290ae568cebe7d6a96d102
EXP <289065406@qq.com>
2022-03-07 22:41:05 +0800

    指定 Premain-Class

--- README.md
+++ README.md
@@ -4,6 +4,23 @@
 
 ------
 
+## 简介 
+
+`-javaagent` 是 java 命令的一个参数。参数 `-javaagent` 可以用于指定一个 jar 包，并且对该 java 包有 2 个要求：
+
+- 这个 jar 包的 `MANIFEST.MF` 文件必须指定 `Premain-Class` 项。
+- `Premain-Class` 指定的那个类必须实现 `premain()` 方法。
+
+`premain()` 方法，从字面上理解，就是运行在 `main()` 函数之前的的类。
+
+当 Java 虚拟机启动时，在执行 `main()` 函数之前，JVM 会先运行 `-javaagent` 所指定 jar 包内 `Premain-Class` 这个类的 `premain()` 方法 。
+
+该包提供了一些工具帮助开发人员在 Java 程序运行时，动态修改系统中的 Class 类型。
+
+
+> 参考文章：《[JavaAgent 使用指南](https://www.cnblogs.com/rickiyang/p/11368932.html)》
+
+
 ## 使用方式
 
 maven 的 `settings.yml` 配置 sonatype 中央仓库：
--- src/main/java/exp/libs/jvm/JVMAgent.java
+++ src/main/java/exp/libs/jvm/JVMAgent.java
@@ -8,27 +8,27 @@ import java.lang.instrument.Instrumentation;
  * </PRE>
  * <br/><B>PROJECT : </B> exp-libs
  * <br/><B>SUPPORT : </B> <a href="https://exp-blog.com" target="_blank">https://exp-blog.com</a>
- * @version   2022-03-06
+ * @version   2022-03-07
  * @author    EXP: exp.lqb@foxmail.com
  * @since     JDK 1.8+
  */
 public class JVMAgent {
 	
-	/** JVM实例代理 */
+	/** JVM 实例代理 */
 	private static Instrumentation instrumentation;
 
 	/**
-	 * 代理程序入口，在main方法执行前执行.
-	 * @param agentArgs 代理程序参数（类比于main参数）
-	 * @param inst JVM实例代理（由JVM自动提供）
+	 * 代理程序入口，在 main 方法执行前执行.
+	 * @param agentArgs 代理程序参数（类比于 main 参数）
+	 * @param inst JVM 实例代理（由 JVM 自动提供）
 	 */
 	public static void premain(String agentArgs, Instrumentation inst) {
 		instrumentation = inst;
 	}
 	
 	/**
-	 * 获取JVM代理对象
-	 * @return JVM代理对象
+	 * 获取 JVM 代理对象
+	 * @return JVM 代理对象
 	 */
 	public static Instrumentation getInstn() {
 		return instrumentation;
