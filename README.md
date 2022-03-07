ad5790b36b7ed73fba633bc90e844b3dda05bbab
EXP <289065406@qq.com>
2022-03-07 22:24:35 +0800

    更新为 Maven 项目

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # jvm-agent
-JVM代理
+
+> JVM 实例代理
+
+------
+
+## 使用方式
+
+maven 的 `settings.yml` 配置 sonatype 中央仓库：
+
+```xml
+<mirror>
+    <id>sonatype</id>
+    <mirrorOf>sonatype</mirrorOf>
+    <url>https://s01.oss.sonatype.org/</url>
+</mirror>
+```
+
+项目的 `pom.xml` 配置构件坐标：
+
+```xml
+<dependency>
+    <groupId>com.exp-blog</groupId>
+    <artifactId>jvm-agent</artifactId>
+    <version>1.0.0-SNAPSHOT</version>
+</dependency>
+```
--- src/main/java/exp/libs/jvm/JVMAgent.java
+++ src/main/java/exp/libs/jvm/JVMAgent.java
@@ -4,13 +4,13 @@ import java.lang.instrument.Instrumentation;
 
 /**
  * <PRE>
- * JVM实例代理类.
+ * JVM 实例代理类.
  * </PRE>
  * <br/><B>PROJECT : </B> exp-libs
- * <br/><B>SUPPORT : </B> <a href="http://www.exp-blog.com" target="_blank">www.exp-blog.com</a> 
- * @version   2015-12-27
- * @author    EXP: 272629724@qq.com
- * @since     jdk版本：jdk1.6
+ * <br/><B>SUPPORT : </B> <a href="https://exp-blog.com" target="_blank">https://exp-blog.com</a>
+ * @version   2022-03-06
+ * @author    EXP: exp.lqb@foxmail.com
+ * @since     JDK 1.8+
  */
 public class JVMAgent {
 	
@@ -20,7 +20,7 @@ public class JVMAgent {
 	/**
 	 * 代理程序入口，在main方法执行前执行.
 	 * @param agentArgs 代理程序参数（类比于main参数）
-	 * @param instP JVM实例代理（由JVM自动提供）
+	 * @param inst JVM实例代理（由JVM自动提供）
 	 */
 	public static void premain(String agentArgs, Instrumentation inst) {
 		instrumentation = inst;
